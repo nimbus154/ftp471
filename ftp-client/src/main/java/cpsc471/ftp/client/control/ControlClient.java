f9166f0397618e9bfbfea0edf75473b677bef097
nimbus154 <nimbus154@gmail.com>
2013-05-18 15:18:00 -0700

    Added ControlClient interface and implementation

--- ftp-client/src/main/java/cpsc471/ftp/client/control/ControlClient.java
+++ ftp-client/src/main/java/cpsc471/ftp/client/control/ControlClient.java
@@ -3,5 +3,25 @@ package cpsc471.ftp.client.control;
 /**
  * Client for the control channel
  */
-public class ControlClient {
+public interface ControlClient {
+
+    /**
+     * List files on server
+     */
+    public void ls();
+
+    /**
+     * Upload a file to the server
+     */
+    public void put();
+
+    /**
+     * Retrieve a file from the server
+     */
+    public void get();
+
+    /**
+     * Terminate connection with server
+     */
+    public void quit();
 }
---
+++ ftp-client/src/main/java/cpsc471/ftp/client/control/ControlClientImpl.java
@@ -0,0 +1,36 @@
+package cpsc471.ftp.client.control;
+
+/**
+ * Implementation of the ControlClient interface
+ */
+public class ControlClientImpl implements ControlClient {
+
+    /**
+     * Create a ControlClient
+     * @param domainName domain name of server to which to connect
+     * @throws Exception if domain name isn't found
+     */
+    public ControlClientImpl(String domainName) throws Exception {
+
+    }
+
+    @Override
+    public void ls() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void put() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void get() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void quit() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
