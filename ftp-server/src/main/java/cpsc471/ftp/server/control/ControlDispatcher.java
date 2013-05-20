524f064d4c124956f3ef8ea0e11fc5e30df1f25f
nimbus154 <nimbus154@gmail.com>
2013-05-20 11:32:59 -0700

    Created ControlDispatcher class and test

---
+++ ftp-server/src/main/java/cpsc471/ftp/server/control/ControlDispatcher.java
@@ -0,0 +1,9 @@
+package cpsc471.ftp.server.control;
+
+/**
+ * Dispatches requests to worker clients
+ */
+public class ControlDispatcher {
+
+
+}
---
+++ ftp-server/src/test/java/cpsc471/ftp/server/control/ControlDispatcherTest.java
@@ -0,0 +1,11 @@
+package cpsc471.ftp.server.control;
+
+import org.testng.annotations.Test;
+
+/**
+ * Tests for ControlDispatcher
+ */
+@Test
+public class ControlDispatcherTest {
+
+}
