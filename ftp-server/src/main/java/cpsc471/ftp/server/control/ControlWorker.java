4d02b40820259fea7e5578fcc1506830df00035c
nimbus154 <nimbus154@gmail.com>
2013-05-20 13:43:07 -0700

    Failing test for invoking ls

--- ftp-server/src/main/java/cpsc471/ftp/server/control/ControlWorker.java
+++ ftp-server/src/main/java/cpsc471/ftp/server/control/ControlWorker.java
@@ -1,7 +1,12 @@
 package cpsc471.ftp.server.control;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
@@ -11,18 +16,88 @@ public class ControlWorker implements Runnable {
 
     private Logger logger = Logger.getLogger(ControlWorker.class);
 
+    private PrintWriter socketWriter;
+
+    private BufferedReader socketReader;
+
+    private Socket socket;
+
     /**
      * Create a worker to handle a connection
      * @param socket a socket representing the connection for this
      *               worker to handle
      */
-    public ControlWorker(Socket socket) {
+    public ControlWorker(Socket socket) throws IOException {
 
+        this.socket = socket;
+        socketWriter = new PrintWriter(socket.getOutputStream());
+        socketReader = new BufferedReader(
+                new InputStreamReader(socket.getInputStream())
+        );
     }
+
+    /**
+     * For testing purposes
+     */
+    public ControlWorker() {
+
+        BasicConfigurator.configure(); // configure log4j
+    }
+
+
 
     @Override
     public void run() {
         //To change body of implemented methods use File | Settings | File Templates.
         logger.info("Servicing connection");
+        ls();
     }
+
+    /**
+     * Handle the ls command
+     */
+    public void ls() {
+
+    }
+
+    public void get(String file, short port) {
+
+    }
+
+    public void put(String file, short port) {
+
+    }
+
+    public void quit() {
+
+    }
+
+    // region Setters and Getters
+
+    public PrintWriter getSocketWriter() {
+        return socketWriter;
+    }
+
+    public void setSocketWriter(PrintWriter socketWriter) {
+        this.socketWriter = socketWriter;
+    }
+
+    public BufferedReader getSocketReader() {
+        return socketReader;
+    }
+
+    public void setSocketReader(BufferedReader socketReader) {
+        this.socketReader = socketReader;
+    }
+
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    // endregion
 }
--- ftp-server/src/test/java/cpsc471/ftp/server/control/ControlWorkerTest.java
+++ ftp-server/src/test/java/cpsc471/ftp/server/control/ControlWorkerTest.java
@@ -1,6 +1,13 @@
 package cpsc471.ftp.server.control;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.io.*;
+import java.net.Socket;
+
+import static org.mockito.Mockito.*;
 
 /**
  * Tests for the ControlWorker server class
@@ -8,5 +15,54 @@ import org.testng.annotations.Test;
 @Test
 public class ControlWorkerTest {
 
+    private Socket socket;
 
+    private ControlWorker worker;
+
+    private ByteArrayOutputStream outputStream;
+
+    private ByteArrayInputStream inputStream;
+
+    // thanks to http://stackoverflow.com/questions/5577274/testing-java-sockets
+    // for the testing pattern
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+        // create a normal instance of the object
+        worker = new ControlWorker();
+
+        // mock the socket
+        socket = mock(Socket.class);
+
+        // when socket method calls, return test objects
+        // this allows for complete control over the internal socket and state
+        outputStream = new ByteArrayOutputStream();
+        when(socket.getOutputStream()).thenReturn(outputStream);
+        when(socket.getInputStream()).thenReturn(inputStream);
+
+        worker.setSocket(socket);
+        worker.setSocketWriter(new PrintWriter(outputStream));
+    }
+
+    /**
+     * Client sends "ls" command to server
+     */
+    public void testLs() {
+
+        // populate a fake socket buffer with the ls command
+        String command = "ls\n";
+        inputStream = new ByteArrayInputStream(command.getBytes());
+        worker.setSocketReader(new BufferedReader(new InputStreamReader(inputStream)));
+
+        // create a worker spy so we can confirm which methods were invoked
+        ControlWorker workerSpy = spy(worker);
+
+        // the method to test
+        workerSpy.run();
+
+        verify(workerSpy, times(1)).ls();
+        Assert.assertEquals(outputStream.toByteArray(), "connecting".getBytes(),
+                "\"connecting\" was not written to output socket");
+    }
 }
