614e58e07beff967ac6d090bc856d7c9a2d9bdb1
nimbus154 <nimbus154@gmail.com>
2013-05-24 09:18:59 -0700

    Made DataChannel abstract with download and upload methods

--- ftp-data-channel/src/main/java/cpsc471/ftp/data/DataChannel.java
+++ ftp-data-channel/src/main/java/cpsc471/ftp/data/DataChannel.java
@@ -1,8 +1,23 @@
 package cpsc471.ftp.data;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Used to transfer files and large messages.
  * The actual file transfer portion of the FTP connection.
  */
-public class DataChannel {
+public abstract class DataChannel {
+
+    /**
+     * Download a file
+     * @param file file to download
+     */
+    public abstract void download(File file) throws IOException;
+
+    /**
+     * Upload a file
+     * @param file file to upload
+     */
+    public abstract void upload(File file) throws IOException;
 }
